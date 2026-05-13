49fe075427e7279bb6c46eb4929d8d005ee7cc51
Aysha149 <ayshaifra149@gmail.com>
2026-05-13 22:24:28 +0530

    react

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,15 @@
-import Courses from "./courses.tsx";
+// import Courses from "./courses.tsx";
+
+import CourseList from "./courseList";
+
 function App() {
 
     return (
     <div>
               <h1 style={{ color: 'green' }}>Course </h1>
 
-      <Courses></Courses>
-      {/* <CourseList></CourseList> */}
+            {/* <Courses></Courses> */}
+ <CourseList></CourseList>
     </div>
   );
 
